0493a5681c5344d965bb67da3bf88a3aa8743892
sks-7 <sachinsinghofficial7@gmail.com>
2024-11-15 17:09:04 +0530

    added env in the port

--- main.go
+++ main.go
@@ -1,33 +1,47 @@
 package main
 
 import (
-	"clockfy_backend/dbconfig"
-	"clockfy_backend/routes"
-	"log"
+    "clockfy_backend/dbconfig"
+    "clockfy_backend/routes"
+    "log"
+    "os" 
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/joho/godotenv"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/cors"
+    "github.com/joho/godotenv"
 )
 
 func main() {
 
-	godotenv.Load()
-	if err := dbconfig.InitDB(); err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
-	}
-	defer dbconfig.CloseDB()
+    godotenv.Load() // Load the environment variables from the .env file
 
-	app := fiber.New()
+    // Connect to the database
+    if err := dbconfig.InitDB(); err != nil {
+        log.Fatalf("Failed to connect to the database: %v", err)
+    }
+    defer dbconfig.CloseDB()
 
-	app.Use(cors.New())
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Running Go Server!")
-	})
+    app := fiber.New()
 
-	routes.RegisterClientRoutes(app)
-	routes.RegisterTagRoutes(app)
-	routes.RegisterProjectRoutes(app)
+    // Middleware
+    app.Use(cors.New())
 
-	log.Fatal(app.Listen(":9002"))
+    // Root route
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Running Go Server!")
+    })
+
+    // Register routes
+    routes.RegisterClientRoutes(app)
+    routes.RegisterTagRoutes(app)
+    routes.RegisterProjectRoutes(app)
+
+    // Get the port from environment variables or use 9002 by default
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "9002" 
+    }
+
+    // Start the server
+    log.Fatal(app.Listen(":" + port))
 }
